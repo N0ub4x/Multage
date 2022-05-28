cfdd20cec57d2f9cc25fcb6b8c33d04d16df8223
N0ub4x <85786075+N0ub4x@users.noreply.github.com>
2022-05-28 13:18:00 +0200

    Add files via upload

---
+++ Emulateur/Mac OS/Applications/Mail_app/Contents/MacOS/exec/dump.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Displays the output of a TinyMCE</title>
+</head>
+<body>
+
+<?
+
+$mailtext = $_REQUEST['elm1'];
+$body = stripslashes($mailtext);
+
+?>
+
+<? echo "$body" ?>
+
+
+
+
+
+</body>
+</html>
+
---
+++ Emulateur/Mac OS/Applications/Mail_app/Contents/MacOS/exec/mail.php
@@ -0,0 +1,20 @@
+<?
+
+// Mail for Mercury.
+// This finally takes care of sending mails, well, from Mail.// Declarate the necessary variables
+
+$mailtext = $_POST['elm1'];
+
+$body = stripslashes($mailtext);
+$to = $_POST['recipient'];$subject = $_POST['subject_line'];
+$from = $_POST['from'];
+$headers = 'From: osx_noreply@se51.net' . "\r\n" .    'Content-Type: text/html; charset=ISO-8859-1' . "\r\n" .
+    'Content-Transfer-Encoding: 7bit' . "\r\n" .
+    'Content-Disposition: inline' . "\r\n" .    'X-Mailer: PHP/' . phpversion();//Check for success/failure of delivery
+mail( $to,$subject,$body,$headers );
+
+// echo "$body";
+
+header( "Location: mailsend.html" );
+
+?>
